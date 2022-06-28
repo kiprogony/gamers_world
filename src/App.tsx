672b33ad2b72c228142136d2e84adff20e888423
Daryl Rogony <darylrogony@gmail.com>
2022-06-28 10:06:14 +0300

    to fetch games using RAWG api

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useCallback, useState } from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import { Header, Cart } from './components';
-import { gameList } from './rawg-api';
+import { gameList } from './api';
 import { Home, GameList, GameDetails, NotFound } from './pages';
 import getPrice from './utils/getPrice';
 import { Game } from './types/Game.types';
