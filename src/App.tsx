5419ccb66a7f53418d3ef12d99454edb0df6d756
Daryl Rogony <darylrogony@gmail.com>
2022-06-28 09:53:19 +0300

    Fit: modified app.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useState } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
+import { Header, Cart } from './components';
+import { gameList } from './rawg-api';
+import { Home, GameList, GameDetails, NotFound } from './pages';
+import getPrice from './utils/getPrice';
+import { Game } from './types/Game.types';
+import './scss/App.scss';
+
+const loadGames = async (search = '') => {
+  const response = await gameList({ page_size: 50, search });
+  let { results } = response;
+  results = results.filter((game) => game.ratings_count > (search ? 50 : 10));
+  results.forEach((game) => game.price = getPrice(game));
+  return results;
+};
 
 function App() {
+  const [cartItems, setCartItems] = useState<Game[]>([]);
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  const location = useLocation();
+  const addToCart = useCallback((game: Game) => {
+    setCartItems([...cartItems, game]);
+  }, [cartItems]);
+  const removeFromCart = useCallback((ids: number[]) => {
+    setCartItems(cartItems.filter((item) => !ids.includes(item.id)));
+  }, [cartItems]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header cartItems={cartItems} setIsCartOpen={setIsCartOpen} />
+      <AnimatePresence exitBeforeEnter>
+        {isCartOpen && (
+          <Cart
+            cartItems={cartItems}
+            setIsCartOpen={setIsCartOpen}
+            removeFromCart={removeFromCart}
+          />
+        )}
+      </AnimatePresence>
+      <AnimatePresence exitBeforeEnter>
+        <Routes location={location} key={location.pathname}>
+          <Route path='*' element={<NotFound />} />
+          <Route
+            path="/"
+            element={<Home loadGames={loadGames} />}
+          />
+          <Route path="games">
+            <Route
+              index
+              element={
+                <GameList
+                  loadGames={loadGames}
+                  cartItems={cartItems}
+                  addToCart={addToCart}
+                />
+              }
+            />
+            <Route
+              path=":gameId"
+              element={
+                <GameDetails
+                  cartItems={cartItems}
+                  addToCart={addToCart}
+                />
+              }
+            />
+          </Route>
+        </Routes>
+      </AnimatePresence>
     </div>
   );
 }
